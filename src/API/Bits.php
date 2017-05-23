fd35565eb34acbee766c5406554cf803f1fd3cf8
wordsongoku <legendsreborn@hotmail.de>
2017-05-23 21:45:57 +0200

    Bits Template added

---
+++ src/API/Bits.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zarlach\TwitchApi\API;
+
+/**
+ * Twitch documentation: https://github.com/justintv/Twitch-API/blob/master/v3_resources/blocks.md.
+ */
+class Bits extends Api
+{
+    
+    // upcoming
+}
